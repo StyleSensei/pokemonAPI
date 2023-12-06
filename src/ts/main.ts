f756e8a02ca2fd781ab145555eeec1cb379b66cf
Patrik Arell <patrikarell@gmail.com>
2023-12-06 13:21:41 +0100

    try to get

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,35 @@
 import './../scss/style.scss';
+import axios from 'axios';
+
+//Lägg till egenskap
+async function searchPokemon (){
+const pokemonData= await axios.get("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=151");
+console.log(pokemonData.data.pokemon);
+
+
+
+}
+
+
+       
+          
+
+
+document.getElementById("searchBtn")?.addEventListener("click", searchPokemon)
+
+
+
+
+
+
+
+
+
+
+
+
+
+//sprites_front_default för att få bild $//
+
+
 
