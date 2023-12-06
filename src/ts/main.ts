67dd30a50109275291c0f51964d22cfc0a81c42f
Patrik Arell <patrikarell@gmail.com>
2023-12-06 14:44:50 +0100

    issues

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,35 +1,41 @@
-import './../scss/style.scss';
-import axios from 'axios';
+import "./../scss/style.scss";
+import axios from "axios";
+
+interface IPokemon {
+  name: string;
+  sprites: string;
+  order: number;
+  gender: string;
+}
+interface IPokemonResponse {
+  results: IPokemon[];
+  url:string;
+}
+// interface IImage {
+//   sprites:string;
+// }
 
 //Lägg till egenskap
-async function searchPokemon (){
-const pokemonData= await axios.get("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=151");
-console.log(pokemonData.data.pokemon);
-
-
-
+async function searchPokemon() {
+  const pokemonData = await axios.get<IPokemonResponse>(
+    "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151"
+  );
+  console.log(pokemonData.data.results);
+//   const pokemonArray = pokemonData.data.results;
+//     for (let i = 0; i < pokemonArray.length; i++) {
+//        console.log(pokemonArray.url);
+           
+for(let i = 0; i < pokemonData.data.results.length; i++){
+  console.log(pokemonData.data.results)
+}
+           
 }
 
+// async function pokemonImage() {
+//   // const pokemonSprite = await axios.get<IPokemon>()
+// }
+document.getElementById("searchBtn")?.addEventListener("click", searchPokemon);
 
-       
-          
-
-
-document.getElementById("searchBtn")?.addEventListener("click", searchPokemon)
-
-
-
-
-
-
-
-
-
-
-
-
-
-//sprites_front_default för att få bild $//
-
+//sprites_front_default för att få[0]()=>{)bild$//
 
 
